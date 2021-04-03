4c514dbbb5abbd4f61a0934e8bdbf80c31522d23
Nick Heyland <nheyland@gmail.com>
2021-04-03 15:51:39 -0400

    hs

--- ap.py
+++ ap.py
@@ -131,6 +131,25 @@ class metarmap:
         brightness = 0.1
         np = neopixel.NeoPixel(board.D18, len(airports), brightness=brightness,
                                auto_write=True, pixel_order=neopixel.GRB)
+        def wheel(pos):
+            if pos < 0 or pos > 255:
+                r = g = b = 0
+            elif pos < 85:
+                r = int(pos * 3)
+                g = int(255 - pos * 3)
+                b = 0
+            elif pos < 170:
+                pos -= 85
+                r = int(255 - pos * 3)
+                g = 0
+                b = int(pos * 3)
+            else:
+                pos -= 170
+                r = 0
+                g = int(pos * 3)
+                b = int(255 - pos * 3)
+            return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
         for j in range(255):
             for i in range(346):
                 pixel_index = (i * 256 // 346) + j
