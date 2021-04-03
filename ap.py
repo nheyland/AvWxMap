433e4000580b583161af198e762e5006053987a9
Nick Heyland <nheyland@gmail.com>
2021-04-03 16:21:52 -0400

    finding light

--- ap.py
+++ ap.py
@@ -133,7 +133,7 @@ class metarmap:
             elif i>-1 and i<5: 
                 # top left
                 np[i] = (52, 107, 235)
-            elif i ==6:
+            elif i ==6 or i==7:
                 np[i] = (255, 255,255 )
             else: 
                 # reds
