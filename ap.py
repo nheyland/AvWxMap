d6b6bce2280f0fdd36a85072ed14553ee266705f
Nick Heyland <nheyland@gmail.com>
2021-04-03 15:47:14 -0400

    red

--- ap.py
+++ ap.py
@@ -132,7 +132,7 @@ class metarmap:
         np = neopixel.NeoPixel(board.D18, len(airports), brightness=brightness,
                                auto_write=True, pixel_order=neopixel.GRB)
         for i in range(0, 346, 1):
-            np[i] = (235, 52, 52)
+            np[i] = (255, 0, 0)
         for i in range(346, 0, -1):
             np[i] = (52, 107, 235)
         for i in range(len(airports)):
