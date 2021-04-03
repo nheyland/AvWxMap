40f282160e4c38441750bab88c90c42d99b62e09
Nick Heyland <nheyland@gmail.com>
2021-04-03 16:31:40 -0400

    finding light

--- ap.py
+++ ap.py
@@ -131,9 +131,8 @@ class metarmap:
             cont = input('Next' + i)
             np[i] = (52, 107, 235)
 
-
     def america(num_leds, np): 
-        letscheck(num_leds, np)
+        self.letscheck(num_leds, np)
         import time
         for i in range(0, num_leds, 1):
             if i >56 and i <124:
