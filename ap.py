51ffb79c41d8a00ed1a83e9777a15e83766e720a
Nick Heyland <nheyland@gmail.com>
2021-04-03 17:50:26 -0400

    finding light

--- ap.py
+++ ap.py
@@ -133,7 +133,7 @@ class metarmap:
         }
         for i in range(0, num_leds, 1):
 
-            if i>343:
+            if i>300:
                 cont = input('Viewing ' + str(i-1))
             if i in colors['red']['leds']:
                 np[i] = (52, 107, 235)
