50fb41d74f5092d5481ef650314e516d9f9b9f37
Nick Heyland <nheyland@gmail.com>
2021-07-03 11:43:01 -0400

    modular

--- ap.py
+++ ap.py
@@ -123,48 +123,51 @@ class info:
                    )
 
 
-
 class metarmap:
-    def america(num_leds, np): 
+    def america(num_leds, np):
         import time
-        colors ={
-            "white":{ "leds": [5, 6, 7, 10, 11, 12, 18, 19, 23, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 49, 50, 51, 53, 54, 55, 124, 125, 126, 127, 130, 131, 132, 136, 137, 139, 140, 141, 142, 146, 147, 148, 149, 155, 156, 157, 158, 161, 162, 163, 166, 167, 168, 172, 175, 176, 177, 178, 179, 180, 181,184, 185, 197, 198, 199, 200, 208, 209, 210, 211, 212, 213, 216, 217, 218, 219, 220, 221, 222, 223, 224, 233, 234, 239, 240, 241, 242, 243, 244, 245, 246,256, 261,263,264,265,268,269,274,275,276,280,281,288,290,291,293,294,295,296,306,307,308,316,321,322,323,324,325,327,328,329,330,331,332,333,334,335,336,338,339,340,341,342]},
-            "blue":{ "leds": [0, 1, 2, 3, 4, 52, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123]}
+        colors = {
+            "white": {"leds": [5, 6, 7, 10, 11, 12, 18, 19, 23, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 49, 50, 51, 53, 54, 55, 124, 125, 126, 127, 130, 131, 132, 136, 137, 139, 140, 141, 142, 146, 147, 148, 149, 155, 156, 157, 158, 161, 162, 163, 166, 167, 168, 172, 175, 176, 177, 178, 179, 180, 181, 184, 185, 197, 198, 199, 200, 208, 209, 210, 211, 212, 213, 216, 217, 218, 219, 220, 221, 222, 223, 224, 233, 234, 239, 240, 241, 242, 243, 244, 245, 246, 256, 261, 263, 264, 265, 268, 269, 274, 275, 276, 280, 281, 288, 290, 291, 293, 294, 295, 296, 306, 307, 308, 316, 321, 322, 323, 324, 325, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 338, 339, 340, 341, 342]},
+            "blue": {"leds": [0, 1, 2, 3, 4, 52, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123]}
         }
         for i in range(0, num_leds, 1):
             if i in colors['blue']['leds']:
                 np[i] = (0, 0, 255)
             elif i in colors['white']['leds']:
                 np[i] = (255, 255, 255)
-            else: 
+            else:
                 np[i] = (255, 0, 0)
         time.sleep(20)
+
+    def metars(airports, np):
+        for i in range(len(airports)):
+            np[i] = wx(airports[i]).color
+
+    def loading(num_leds, np):
+        for i in range(0, num_leds, 1):
+            np[i] = (255, 0, 0)
+        for i in range(num_leds, 0, -1):
+            np[i] = (52, 107, 235)
+
     def __init__(self, airports):
         import board
         import neopixel
         import time
-        num_leds= 346
-        brightness = 0.1
+        schema = r.get(
+            "https://raw.githubusercontent.com/nheyland/AvWxMap/master/config.json?flush_cache=True").json()
+        num_leds = 346
+        brightness = schema.brightness
         np = neopixel.NeoPixel(board.D18, len(airports), brightness=brightness,
                                auto_write=True, pixel_order=neopixel.GRB)
+
         def run():
             metarmap.america(num_leds, np)
-            for i in range(0, num_leds, 1):
-                np[i] = (255, 0, 0)
-            for i in range(num_leds, 0, -1):
-                np[i] = (52, 107, 235)
-            for i in range(len(airports)):
-                np[i] = wx(airports[i]).color
-            print('sleep15')
+            metarmap.loading(num_leds, np)
+            metarmap.metars(airports, np)
             time.sleep(900)
-            print('sleep15end')
-
             run()
         run()
         return
 
     def __repr__(self):
         return str('Unsurpported')
-
-
-
