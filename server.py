0af70afd11440d710dd9f735fcc7840ade7b6be4
Nick Heyland <nheyland@gmail.com>
2021-07-05 11:37:37 -0400

    cors

--- server.py
+++ server.py
@@ -1,39 +1,48 @@
 from ap import *
 from flask import jsonify, Flask, request
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/america")
+@cross_origin()
 def america():
     metarmap().america()
     return "<p>success<P>"
 
 
 @app.route("/metars")
+@cross_origin()
 def metar():
     metarmap().metars()
     return "<p>success<P>"
 
 
 @app.route("/test")
+@cross_origin()
 def test():
     metarmap().test()
     return "<p>success<P>"
 
 
 @app.route("/red")
+@cross_origin()
 def red():
     metarmap().red()
     return "<p>success<P>"
 
 
 @app.route("/blue")
+@cross_origin()
 def blue():
     metarmap().blue()
     return "<p>success<P>"
 
 
 @app.route("/green")
+@cross_origin()
 def green():
     metarmap().green()
     return "<p>success<P>"
