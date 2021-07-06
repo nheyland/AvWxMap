5b283cbd226d7c1840fc45a1af4404955ffa1924
Nick Heyland <nheyland@gmail.com>
2021-07-06 16:10:19 -0400

    ad

--- smarthome.py
+++ smarthome.py
@@ -2,10 +2,7 @@ import pywemo
 
 
 class Plug:
-    def __init__(self):
-        self.url = pywemo.setup_url_for_address("192.168.0.35", None)
-        pass
-
-    def toggle(self):
-        device = pywemo.discovery.device_from_description(self.url)
+    def toggle():
+        url = pywemo.setup_url_for_address("192.168.0.35", None)
+        device = pywemo.discovery.device_from_description(url)
         device.toggle()
